696f0704d43ed70a46ba1bb81ff055fad08eca6f
Siluni Keerthiratne <siluni@keerthiratne.com>
2023-11-27 12:11:57 +0530

    Css styling v2 pt 4???

--- src/main/java/SCMS/Controllers/EventContorller.java
+++ src/main/java/SCMS/Controllers/EventContorller.java
@@ -26,64 +26,68 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class EventContorller implements Initializable { //PUT THE BACK BUTTON
-    HelloApplication  helloApplicationInstance = new HelloApplication();
-    @FXML
-    TextField eventTitle;
-    @FXML
-    DatePicker eventDate;
-    @FXML
-    TextField eventVenue;
-    @FXML
-    TextField eventDescription;
-    @FXML
-    ChoiceBox<String> eventTypeChoiceBox;
-    String[] typeList = {"Event", "Meeting", "Activity"};
-    String type = "";
-    private static Connection connections = SCMSEnvironment.getInstance().makeSqlDBConnection();
+// Controller for managing events
+public class EventContorller implements Initializable {
+    private HelloApplication helloApplicationInstance = new HelloApplication();
 
+    // FXML elements
+    @FXML
+    private TextField eventTitle;
+    @FXML
+    private DatePicker eventDate;
+    @FXML
+    private TextField eventVenue;
+    @FXML
+    private TextField eventDescription;
+    @FXML
+    private ChoiceBox<String> eventTypeChoiceBox;
     @FXML
     private Button backButtonCDD;
     @FXML
-    TextField clubIdTextBoxData;
+    private TextField clubIdTextBoxData;
     @FXML
-    TextField timeHourTextBoxData;
+    private TextField timeHourTextBoxData;
     @FXML
-    TextField timeMinuteTextBoxData;
-    LocalDateTime dateTime;
-    String errorMessage = "";
+    private TextField timeMinuteTextBoxData;
     @FXML
-    Label errorMessageLabel;
-    public String buttonText;
+    private Label errorMessageLabel;
+
+    // Date and time variables
+    private LocalDateTime dateTime;
+    private String errorMessage = "";
+
+    // Other variables
+    private String[] typeList = {"Event", "Meeting", "Activity"};
+    private String type = "";
+    private static Connection connections = SCMSEnvironment.getInstance().makeSqlDBConnection();
+    private String buttonText;
     private Club club;
     private String adId;
 
-    public void gettingInformation(Club club,String advisorId){
-        this.club=club;
-        this.adId=advisorId;
+    // Method to pass information to the controller
+    public void gettingInformation(Club club, String advisorId) {
+        this.club = club;
+        this.adId = advisorId;
     }
 
-    public void  backButtonCDD  (ActionEvent event) throws Exception{
-        String fileName = "/SCMS/FxmlFiles/PressClub.fxml";      //open the page
+    // Back button action
+    public void backButtonCDD(ActionEvent event) throws Exception {
+        String fileName = "/SCMS/FxmlFiles/PressClub.fxml";
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
         Parent root = loader.load();
         buttonText = club.getName();
-
         PressClubController pcc = loader.getController();
-        pcc.setWelcomeText(buttonText,adId);
-
+        pcc.setWelcomeText(buttonText, adId);
         Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
 
-    public static HashMap<String, String> SQLReader(String yearMonth){
-//        yearMonth format => "2023-11"
+    // SQL reader method to get event counts
+    public static HashMap<String, String> SQLReader(String yearMonth) {
         try {
             String sqlQuery = "SELECT typeOfClubFunction, COUNT(*) AS numberOfEvents FROM Event WHERE DATE_FORMAT(dateTime, '%Y-%m') = ? GROUP BY typeOfClubFunction";
-
-            // Create a PreparedStatement
             PreparedStatement statement = connections.prepareStatement(sqlQuery);
             statement.setString(1, yearMonth);
             ResultSet resultSet = statement.executeQuery();
@@ -102,36 +106,31 @@ public class EventContorller implements Initializable { //PUT THE BACK BUTTON
         return null;
     }
 
+    // Validate time and type of event
     public String validateTimeAndTypeOfEvent(LocalDate date, String hour, String minute, String typ) {
-
         Integer eventHour;
         Integer eventMinute;
         try {
-             eventHour = Integer.parseInt(hour);
-             eventMinute = Integer.parseInt(minute);
-
-
-        }
-        catch (NumberFormatException e) {
+            eventHour = Integer.parseInt(hour);
+            eventMinute = Integer.parseInt(minute);
+        } catch (NumberFormatException e) {
             errorMessage = "Time should be a number";
             errorMessageLabel.setText(errorMessage);
             return errorMessage;
         }
 
-        if ((eventHour > 18 || eventHour<=4)||(eventMinute > 59 || eventHour<=0)){
-                errorMessage = "Any club activity should be held between 4.00 to 18.00";
-                errorMessageLabel.setText(errorMessage);
-                return errorMessage;
-            }
-        if (eventMinute > 59 || eventHour<=0){
+        if ((eventHour > 18 || eventHour <= 4) || (eventMinute > 59 || eventHour <= 0)) {
+            errorMessage = "Any club activity should be held between 4.00 to 18.00";
+            errorMessageLabel.setText(errorMessage);
+            return errorMessage;
+        }
+        if (eventMinute > 59 || eventHour <= 0) {
             errorMessage = "Minutes should be within 0-59";
             errorMessageLabel.setText(errorMessage);
             return errorMessage;
         }
         LocalTime time = LocalTime.of(eventHour, eventMinute);
         dateTime = date.atTime(time);
-
-
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
         String formattedDateTime = dateTime.format(formatter);
@@ -140,103 +139,79 @@ public class EventContorller implements Initializable { //PUT THE BACK BUTTON
         int noOfEvents = 0;
         int noOfMeetings = 0;
         int noOfActivities = 0;
-        if(eventCount.containsKey("Event")){
+        if (eventCount.containsKey("Event")) {
             noOfEvents = Integer.parseInt(eventCount.get("Event"));
         }
-        if(eventCount.containsKey("Meeting")){
+        if (eventCount.containsKey("Meeting")) {
             noOfMeetings = Integer.parseInt(eventCount.get("Meeting"));
         }
-        if(eventCount.containsKey("Activity")){
+        if (eventCount.containsKey("Activity")) {
             noOfActivities = Integer.parseInt(eventCount.get("Activity"));
         }
 
-        if(type.equalsIgnoreCase("Event") && noOfEvents==2){
+        if (type.equalsIgnoreCase("Event") && noOfEvents == 2) {
             errorMessage = "Only 2 events per month can be created.";
             return errorMessage;
         }
-        if(type.equalsIgnoreCase("Meeting") && noOfMeetings==1){
+        if (type.equalsIgnoreCase("Meeting") && noOfMeetings == 1) {
             errorMessage = "Only 1 meeting per month can be created.";
             return errorMessage;
         }
-        if(type.equalsIgnoreCase("Activity") && noOfActivities==4){
+        if (type.equalsIgnoreCase("Activity") && noOfActivities == 4) {
             errorMessage = "Only 2 activity per month can be created.";
             return errorMessage;
         }
         return errorMessage;
     }
 
-
-    public String validateTextFileds(String title, String venue, String description, String type){
-        if(title.isEmpty() || venue.isEmpty()  || description.isEmpty()  || type.isEmpty()){
+    // Validate text fields for empty values
+    public String validateTextFileds(String title, String venue, String description, String type) {
+        if (title.isEmpty() || venue.isEmpty() || description.isEmpty() || type.isEmpty()) {
             errorMessage = "Fields cannot be empty";
-
-
         }
         return errorMessage;
     }
 
+    // Initialization method
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         eventTypeChoiceBox.getItems().addAll(typeList);
         eventTypeChoiceBox.setOnAction(this::onCreateNewEvent);
-
     }
 
+    // Create new event action
     public void onCreateNewEvent(ActionEvent event) {
         errorMessage = "";
         String title = eventTitle.getText();
         String venue = eventVenue.getText();
-        String description = eventDescription.getText();;
+        String description = eventDescription.getText();
         type = eventTypeChoiceBox.getValue();
 
-
-        errorMessage = (validateTextFileds(title, venue, description,type));
+        errorMessage = (validateTextFileds(title, venue, description, type));
         errorMessageLabel.setText(errorMessage);
         System.out.println(errorMessage);
 
-            if(errorMessage.equalsIgnoreCase("")){
-                LocalDate date = eventDate.getValue();
-                String strHour = timeHourTextBoxData.getText();
-                String strMinute = timeMinuteTextBoxData.getText();
-                errorMessage = validateTimeAndTypeOfEvent(date,strHour,strMinute,type);
-                errorMessageLabel.setText(errorMessage);
-                System.out.println(errorMessage);
+        if (errorMessage.equalsIgnoreCase("")) {
+            LocalDate date = eventDate.getValue();
+            String strHour = timeHourTextBoxData.getText();
+            String strMinute = timeMinuteTextBoxData.getText();
+            errorMessage = validateTimeAndTypeOfEvent(date, strHour, strMinute, type);
+            errorMessageLabel.setText(errorMessage);
+            System.out.println(errorMessage);
 
+            if (errorMessage.equalsIgnoreCase("")) {
+                eventTitle.setText("");
+                eventVenue.setText("");
+                eventDescription.setText("");
+                timeHourTextBoxData.setText("");
+                timeMinuteTextBoxData.setText("");
+                eventDate.setValue(null);
+                eventTypeChoiceBox.setValue(null);
+                errorMessageLabel.setText("");
 
-                if (errorMessage.equalsIgnoreCase("")){
-                    eventTitle.setText("");
-                    eventVenue.setText("");
-                    eventDescription.setText("");
-
-                    timeHourTextBoxData.setText("");
-                    timeMinuteTextBoxData.setText("");
-                    eventDate.setValue(null);
-                    eventTypeChoiceBox.setValue(null);
-                    errorMessageLabel.setText("");
-
-
-                    Event newEvent = new Event(title, dateTime,venue, type, description,new Button());
-                    newEvent.writeEventsToSqlDB(club.getClubId());
-                }
-
+                Event newEvent = new Event(title, dateTime, venue, type, description, new Button());
+                newEvent.writeEventsToSqlDB(club.getClubId());
+            }
         }
-
-    }}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}
--- src/main/java/SCMS/Objects/Event.java
+++ src/main/java/SCMS/Objects/Event.java
@@ -7,8 +7,11 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+// Event class represents an event in the system
 public class Event {
+    // Establish a connection to the SQL database
     private Connection connections = SCMSEnvironment.getInstance().makeSqlDBConnection();
+    // Event properties
     private String eventId;
     private String title;
     private LocalDateTime dateTime;
@@ -16,179 +19,156 @@ public class Event {
     private String type;
     private String description;
     private ArrayList<Student> studentsWhoJoined = new ArrayList<>();
-    //i made a change here check
     private Button button;
+
+    // Constructor for creating an event without a button
     public Event(String title, LocalDateTime dateTime, String venue, String type, String description) {
-
-        this.eventId = "E" + String.valueOf(getNoOfEventsFromSqlDB()+1);
-        this.title = title;
-        this.dateTime = dateTime;
-        this.venue = venue;
-        this.type = type;
-        this.description = description;
-        //this.button = new Button("mark attendance");
-    }
-    public Event(String title, LocalDateTime dateTime, String venue, String type, String description, Button button) {
-
         this.eventId = "E" + String.valueOf(getNoOfEventsFromSqlDB() + 1);
         this.title = title;
         this.dateTime = dateTime;
         this.venue = venue;
         this.type = type;
         this.description = description;
+    }
 
-        this.button =button;
+    // Constructor for creating an event with a button
+    public Event(String title, LocalDateTime dateTime, String venue, String type, String description, Button button) {
+        this.eventId = "E" + String.valueOf(getNoOfEventsFromSqlDB() + 1);
+        this.title = title;
+        this.dateTime = dateTime;
+        this.venue = venue;
+        this.type = type;
+        this.description = description;
+        this.button = button;
         this.button.setText("mark stuff");
     }
+
+    // Constructor for creating an event with a specified eventId and button
     public Event(String eventId, String title, LocalDateTime dateTime, String venue, String type, String description, Button button) {
-
         this.eventId = eventId;
         this.title = title;
         this.dateTime = dateTime;
         this.venue = venue;
         this.type = type;
         this.description = description;
-        this.button =button;
+        this.button = button;
         this.button.setText("mark stuff");
-
     }
-    public Event(String eventId, String title, LocalDateTime dateTime, String venue, String type, String description, Button button,ArrayList<Student> studentsWhoJoined) {
 
+    // Constructor for creating an event with specified eventId, button, and studentsWhoJoined
+    public Event(String eventId, String title, LocalDateTime dateTime, String venue, String type, String description, Button button, ArrayList<Student> studentsWhoJoined) {
         this.eventId = eventId;
         this.title = title;
         this.dateTime = dateTime;
         this.venue = venue;
         this.type = type;
         this.description = description;
-        this.button =button;
+        this.button = button;
         this.button.setText("mark stuff");
-        this.studentsWhoJoined=studentsWhoJoined;
-
+        this.studentsWhoJoined = studentsWhoJoined;
     }
 
+    // Getter for studentsWhoJoined
     public ArrayList<Student> getStudentsWhoJoined() {
         return studentsWhoJoined;
     }
 
+    // Setter for studentsWhoJoined
     public void setStudentsWhoJoined(ArrayList<Student> studentsWhoJoined) {
         this.studentsWhoJoined = studentsWhoJoined;
     }
 
+    // Getter for the button
     public Button getButton() {
         return button;
     }
 
+    // Setter for the button
     public void setButton(Button button) {
         this.button = button;
     }
 
-    public Event(String funcType, String title, LocalDateTime dateTime, String venue){
+    // Constructor for creating an event with minimal details
+    public Event(String funcType, String title, LocalDateTime dateTime, String venue) {
         this.type = funcType;
         this.title = title;
         this.dateTime = dateTime;
         this.venue = venue;
     }
 
+    // Getter for eventId
     public String getEventId() {
         return eventId;
     }
 
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
-    }
-
+    // Getter for title
     public String getTitle() {
         return title;
     }
 
+    // Setter for title
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // Getter for dateTime
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
+    // Getter for venue
     public String getVenue() {
         return venue;
     }
 
+    // Setter for venue
     public void setVenue(String venue) {
         this.venue = venue;
     }
 
+    // Getter for type
     public String getType() {
         return type;
     }
 
+    // Setter for type
     public void setType(String type) {
         this.type = type;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-
+    // Method to get the number of events from the SQL database
     public Integer getNoOfEventsFromSqlDB() {
-
         String query = "SELECT COUNT(*) AS record_count FROM Event";
-
         try (Statement statement = connections.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)){
+             ResultSet resultSet = statement.executeQuery(query)) {
             int recordCount = 0;
             if (resultSet.next()) {
                 recordCount = resultSet.getInt("record_count");
                 System.out.println("Total number of records in the table: " + recordCount);
-
             }
-            return (recordCount);
+            return recordCount;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
-            // Handle the exception here
         }
     }
 
-    public void writeEventsToSqlDB(String clubId){
+    // Method to write events to the SQL database
+    public void writeEventsToSqlDB(String clubId) {
         try {
             String query = "INSERT INTO Event (eventId, title, dateTime, venue, typeOfClubFunction, description, clubId) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-            // Create a PreparedStatement
             PreparedStatement statement = connections.prepareStatement(query);
-
-            // Set the values for the parameters in the SQL query
             statement.setString(1, this.eventId);
             statement.setString(2, this.title);
-            statement.setObject(3, this.dateTime); // LocalDateTime to SQL DATETIME
+            statement.setObject(3, this.dateTime);
             statement.setString(4, this.venue);
             statement.setString(5, this.type);
             statement.setString(6, this.description);
             statement.setString(7, clubId);
-
-
             statement.executeUpdate();
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
     }
-
-
-
 }
